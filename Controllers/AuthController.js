08830be89baef8be07602c10bf7ae3add52a9fa4
ZainKeepsCode <ahmadrao925@gmail.com>
2022-05-29 21:50:58 -0700

    Sever Side Social Media

---
+++ Controllers/AuthController.js
@@ -0,0 +1,49 @@
+import UserModel from "../Models/userModel.js";
+import bcrypt from "bcrypt";
+
+// Registering a new User
+export const registerUser = async (req, res) => {
+  const { username, password, firstname, lastname } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, salt);
+
+  const newUser = new UserModel({
+    username,
+    password: hashedPass,
+    firstname,
+    lastname,
+  });
+
+  try {
+    await newUser.save();
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// login User
+
+export const loginUser = async (req, res) => {
+    const {username, password} = req.body
+
+    try {
+        const user = await UserModel.findOne({username: username})
+
+
+        if(user)
+        {
+            const validity = await bcrypt.compare(password, user.password)
+
+
+            validity? res.status(200).json(user): res.status(400).json("Wrong Password")
+        }
+        else{
+            res.status(404).json("User does not exists")
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import AuthRoute from './Routes/AuthRoute.js'
+import UserRoute from './Routes/UserRoute.js'
+import PostRoute from './Routes/PostRoute.js'
+
+// Routes
+
+const app = express();
+
+
+// Middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Listening at ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error));
+
+
+  // usage of routes
+  app.use('/auth', AuthRoute)
+  app.use('/user', UserRoute)
+  app.use('/post', PostRoute)
