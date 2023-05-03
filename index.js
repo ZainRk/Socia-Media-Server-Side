a209b6c6e51a436aef09215229ef984eb9e744f8
Laditya Gogoi <108052802+Lobi29@users.noreply.github.com>
2023-05-03 21:48:38 +0530

    updating mongoose package code with mongodb package
          Because a lot changes has comes into the mongoose since you last commit into this project. So, I am trying to make your project error/bug free as much as possible.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from "dotenv";
 import AuthRoute from './Routes/AuthRoute.js'
 import UserRoute from './Routes/UserRoute.js'
@@ -17,20 +17,25 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`Listening at ${process.env.PORT}`)
-    )
-  )
-  .catch((error) => console.log(error));
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGO_DB, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    await client.connect()
+        .then(() =>
+            app.listen(process.env.PORT, () =>
+                console.log(`listening on port ${process.env.PORT}`)));
+}
+run().catch(console.dir)
 
 
-  // usage of routes
-  app.use('/auth', AuthRoute)
-  app.use('/user', UserRoute)
-  app.use('/post', PostRoute)
+// usage of routes
+app.use('/auth', AuthRoute)
+app.use('/user', UserRoute)
+app.use('/post', PostRoute)
